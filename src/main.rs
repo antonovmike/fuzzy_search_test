54f105f4ef06930ba4819dd7516a11ff19afe90b
antonovmike <antonovma@outlook.com>
2023-03-28 12:28:43 +0500

    encoding issue

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,12 @@ fn main() {
     let long_string = "INSERT [dbo].[GOOD] ([ID], [NAME], [PRNAME], [EDIZM], [PRODUCER], [NMN], [GOOD], [GROUPS], [GROUPS_CH], [GRKOL], [MANDATORY], [BARCODE], [MX], [REC], [STATE], [DATACHANGE], [ANATACIA], [SUM_GOOD], [FOM_GOOD], [TRADENAME], [DOSAGE], [FORM], [REFERENCE], [ART], [SENDSPR], [FOM_SUMMARY_GROUPS], [MED], [CLASSCODE], [ATXID], [MNNFR], [Label], [UnitCode], [WHTYPE], [TESTDATE], [CLASSCODEOLD]) VALUES (313911, N'\" # \" !130', N'.$ , !(', N'', 55556, 3685, NULL, 40, 185, 130, NULL, N'3629002748139', N'', 0, 3, CAST(N'2023-02-09T09:01:07.057' AS DateTime), N'', NULL, 6924, N'', N'', N'', NULL, 0, 1, NULL, 0, N'03004066004025012', 588, 1003685, 0, N'1146307', 1, CAST(N'2023-02-09T09:01:06.380' AS DateTime), NULL)";
     let shorter_string = &long_string[398..];
     let parts = shorter_string.split(", N'");
+
+    let mut index = 0;
     for part in parts {
-        println!("{}", part)
+        if index == 1 || index == 2 {
+            println!("{}", part)
+        }
+        index += 1
     }
 }
