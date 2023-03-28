659897295e815aeb5c7009d9fb5535074a64f5c0
antonovmike <antonovma@outlook.com>
2023-03-28 14:50:48 +0500

    encoding and split issue

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,15 @@ fn main() {
 
     for (index, line) in reader.lines().enumerate() {
         if index > 60 && index < 64 {
-            lines.push(line.unwrap())
+            let sliced = &line.unwrap()[..];
+            lines.push(sliced.to_string())
         }
     }
 
     for i in lines {
-        let shorter_string = &i[398..];
-        let parts = shorter_string.split(", N'");
+        let shorter_string = &i[816..];
+        // let parts = shorter_string.split(", N'");
+        let parts = shorter_string.split("N");
         let mut index = 0;
         for part in parts {
             if index == 0 || index == 2 {
@@ -25,6 +27,7 @@ fn main() {
             index += 1
         }
     }
+
     // let long_string = "INSERT [dbo].[GOOD] ([ID], [NAME], [PRNAME], [EDIZM], [PRODUCER], [NMN], [GOOD], [GROUPS], [GROUPS_CH], [GRKOL], [MANDATORY], [BARCODE], [MX], [REC], [STATE], [DATACHANGE], [ANATACIA], [SUM_GOOD], [FOM_GOOD], [TRADENAME], [DOSAGE], [FORM], [REFERENCE], [ART], [SENDSPR], [FOM_SUMMARY_GROUPS], [MED], [CLASSCODE], [ATXID], [MNNFR], [Label], [UnitCode], [WHTYPE], [TESTDATE], [CLASSCODEOLD]) VALUES (313911, N'\" # \" !130', N'.$ , !(', N'', 55556, 3685, NULL, 40, 185, 130, NULL, N'3629002748139', N'', 0, 3, CAST(N'2023-02-09T09:01:07.057' AS DateTime), N'', NULL, 6924, N'', N'', N'', NULL, 0, 1, NULL, 0, N'03004066004025012', 588, 1003685, 0, N'1146307', 1, CAST(N'2023-02-09T09:01:06.380' AS DateTime), NULL)";
     // let shorter_string = &long_string[398..];
     // let parts = shorter_string.split(", N'");
