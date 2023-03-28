9b85e0ed8affd84230c285b76e79e53556152d5e
antonovmike <antonovma@outlook.com>
2023-03-28 11:18:15 +0500

    text file access

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let text_file = "example.sql";
+    let file = File::open(text_file).unwrap();
+    let reader = BufReader::new(file);
+
+    for (index, line) in reader.lines().enumerate() {
+        if index > 50 && index < 55 {
+            let line = line.unwrap();
+            println!("{}\n{}", index + 1, line);
+        }
+    }
 }
