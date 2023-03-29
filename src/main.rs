9c448f88ddd7f17f8db811b0e331d5636d3d9ccd
antonovmike <antonovma@outlook.com>
2023-03-29 07:19:09 +0500

    panic if > 130 records

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
     let mut lines: Vec<String> = vec![];
 
     for (index, line) in s.lines().enumerate() {
-        if index > 60 && index < 64 {
+        if index > 60 && index < 190 {
             let sliced = line;
             lines.push(sliced.to_string())
         }
@@ -24,27 +24,38 @@ fn main() {
     let mut search_id = 0;
     let mut catalog: Vec<(u32, String)> = vec![];
 
+    #[allow(unused_assignments)]
     for i in lines.clone() {
-        let shorter_string = &i[398..];
+        // Exclude useless data
+        let mut shorter_string = "";
+        if i.len() > 398 {
+            shorter_string = &i[398..]
+        }
         let parts = shorter_string.split("N");
         let mut index = 0;
         for part in parts {
-            // Remove 1st n 2nd chars
+            // Remove 1st and last chars
             let mut chars = part.chars();
             chars.next();
             chars.next_back();
             let raw_str = chars.as_str();
-            // remove last 2 chars
-            let pure_str = raw_str.split_at(raw_str.len() - 2);
+            // Exclude useless str
+            let mut pure_str = ("", "");
+            if raw_str.len() > 2 {
+                // Remove last 2 chars
+                pure_str = raw_str.split_at(raw_str.len() - 2)
+            } else {
+                continue;
+            }
             if index == 1 {
-                println!("{}\t{}", search_id, pure_str.0);
+                // println!("{}\t{}", search_id, pure_str.0);
                 products.push(pure_str.0.to_string());
                 engine.insert(search_id, pure_str.0);
                 catalog.push((search_id, pure_str.0.to_string()));
                 search_id += 1;
             }
             if index == 2 {
-                println!("{}\t{}", search_id, pure_str.0);
+                // println!("{}\t{}", search_id, pure_str.0);
                 products.push(pure_str.0.to_string());
                 engine.insert(search_id, pure_str.0);
                 catalog.push((search_id, pure_str.0.to_string()));
@@ -83,10 +94,5 @@ fn main() {
             }
             println!("Найдено: {}", final_string);
         }
-        // if products.contains(&input) {
-        //     println!("Найдено")
-        // } else {
-        //     println!("Не найдено")
-        // }
     }
 }
