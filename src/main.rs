de6ec1535afc105fa2c4ba1babdc74dafc9e6b15
antonovmike <antonovma@outlook.com>
2023-03-29 17:42:47 +0500

    refactor

--- src/main.rs
+++ src/main.rs
@@ -4,30 +4,7 @@ use std::io::{self, Write};
 use std::io::{BufRead, BufReader};
 
 fn main() {
-    let text_file = "utf8_dbo.GOOD.Table.sql";
-    let file = File::open(text_file).unwrap();
-
-    // let mut engine: SimSearch<u32> = SimSearch::new();
-    let mut engine: SimSearch<u32> = SimSearch::new_with(SearchOptions::new().threshold(0.9));
-    let mut search_id = 0;
-
-    let mut catalog: Vec<(u32, String)> = vec![];
-
-    BufReader::new(file)
-        .lines()
-        .map(|l| l.unwrap())
-        .filter(|l| l.starts_with("INSERT"))
-        .map(|l| l[398..].to_string())
-        .map(|l| {
-            let name = l.split("N'").nth(1).unwrap();
-            name[0..name.len() - 3].to_owned()
-        })
-        .for_each(|name| {
-            engine.insert(search_id, &name);
-            catalog.push((search_id, name.to_string()));
-
-            search_id += 1;
-        });
+    let (catalog, engine) = load();
 
     loop {
         print!("Текст для поиска: ");
@@ -38,16 +15,16 @@ fn main() {
 
         input = input.trim().to_string();
 
-        let results: Vec<u32> = engine.search(&input);
+        let mut results: Vec<u32> = engine.search(&input);
 
         let total = results.len();
         if total == 0 {
             println!("Нет совпадений");
             continue;
         }
-        // if total > 10 {
-        //     results.drain(10..);
-        // }
+        if total > 10 {
+            results.drain(10..);
+        }
         for index in results {
             println!("{}, {:?}", index, catalog[index as usize].1)
         }
@@ -55,23 +32,15 @@ fn main() {
     }
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn test_trim() {
-//         assert_eq!("верблжй", trimmer("верблжй ".to_string()));
-//     }
-// }
-
-#[test]
-fn mistape_1() {
+fn load() -> (Vec<(u32, String)>, SimSearch<u32>) {
     let text_file = "utf8_dbo.GOOD.Table.sql";
     let file = File::open(text_file).unwrap();
+
     let mut engine: SimSearch<u32> = SimSearch::new_with(SearchOptions::new().threshold(0.9));
     let mut search_id = 0;
+
     let mut catalog: Vec<(u32, String)> = vec![];
+
     BufReader::new(file)
         .lines()
         .map(|l| l.unwrap())
@@ -84,49 +53,46 @@ fn mistape_1() {
         .for_each(|name| {
             engine.insert(search_id, &name);
             catalog.push((search_id, name.to_string()));
+
             search_id += 1;
         });
 
-    let input = "верблжй";
-    let results: Vec<u32> = engine.search(&input);
-    let total = results.len();
-    assert_eq!(13, total)
+    (catalog, engine)
 }
 
-#[test]
-fn mistape_2() {
-    let text_file = "utf8_dbo.GOOD.Table.sql";
-    let file = File::open(text_file).unwrap();
-    let mut engine: SimSearch<u32> = SimSearch::new_with(SearchOptions::new().threshold(0.9));
-    let mut search_id = 0;
-    let mut catalog: Vec<(u32, String)> = vec![];
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    BufReader::new(file)
-        .lines()
-        .map(|l| l.unwrap())
-        .filter(|l| l.starts_with("INSERT"))
-        .map(|l| l[398..].to_string())
-        .map(|l| {
-            let name = l.split("N'").nth(1).unwrap();
-            name[0..name.len() - 3].to_owned()
-        })
-        .for_each(|name| {
-            engine.insert(search_id, &name);
-            catalog.push((search_id, name.to_string()));
+    // #[test]
+    // fn test_trim() {
+    //     assert_eq!("верблжй", trimmer("верблжй ".to_string()));
+    // }
+    #[test]
+    fn mistape_1() {
+        let (_catalog, engine) = load();
 
-            search_id += 1;
-        });
-
-    let input = "верблжй";
-    engine.search(&input);
-
-    let mut qwe = false;
-    for i in catalog.clone() {
-        if i.0 == 1943 {
-            println!("TEST {:?}", i);
-            qwe = true
-        }
+        let input = "верблжй";
+        let results: Vec<u32> = engine.search(&input);
+        let total = results.len();
+        assert_eq!(13, total)
     }
 
-    assert_eq!(true, qwe)
+    #[test]
+    fn mistape_2() {
+        let (catalog, engine) = load();
+
+        let input = "верблжй";
+        engine.search(&input);
+
+        let mut qwe = false;
+        for i in catalog.clone() {
+            if i.0 == 1943 {
+                println!("TEST {:?}", i);
+                qwe = true
+            }
+        }
+
+        assert_eq!(true, qwe)
+    }
 }
