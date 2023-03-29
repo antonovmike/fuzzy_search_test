fab0938e7b796c858d5bbd3007e5b539db966045
antonovmike <antonovma@outlook.com>
2023-03-29 11:55:55 +0500

    map cut string at 398

--- src/main.rs
+++ src/main.rs
@@ -7,52 +7,37 @@ fn main() {
     let text_file = "utf8_dbo.GOOD.Table.sql";
     let file = File::open(text_file).unwrap();
 
-    let mut engine: SimSearch<u32> = SimSearch::new();
-    let mut catalog: Vec<(u32, String)> = vec![];
-    let mut search_id = 0;
-
-    BufReader::new(file)
+    let it = BufReader::new(file)
         .lines()
-        // .into_iter()
+        .into_iter()
         .map(|l| l.unwrap())
         .filter(|l| l.starts_with("INSERT"))
-        .map(|l| l[398..].to_string())
-        .map(|l| {
-            let name = l.split("N'").collect::<Vec<_>>();
-            println!("{}", name[1]);
-            name[0..name[1].len() - 29].join("")
-        })
-        .for_each(|name| {
-            engine.insert(search_id, &name);
-            catalog.push((search_id, name));
-
-            search_id += 1;
-        });
+        .map(|l| l[398..].to_owned());
     // .take(500);
 
-    // let mut engine: SimSearch<u32> = SimSearch::new();
-    // let mut search_id = 0;
+    let mut engine: SimSearch<u32> = SimSearch::new();
+    let mut search_id = 0;
 
-    // let mut catalog: Vec<(u32, String)> = vec![];
+    let mut catalog: Vec<(u32, String)> = vec![];
 
-    // for i in lines.clone() {
-    //     let parts = i.split("N'");
-    //     for (i, part) in parts.enumerate() {
-    //         if i == 1 {
-    //             // remove last 3 chars
-    //             let name = part[0..part.len() - 3].to_string();
-    //             println!("{}\t{}", search_id, name);
+    for i in it {
+        let parts = i.split("N'");
+        for (i, part) in parts.enumerate() {
+            if i == 1 {
+                // remove last 3 chars
+                let name = part[0..part.len() - 3].to_string();
+                println!("{}\t{}", search_id, name);
 
-    //             engine.insert(search_id, &name);
-    //             catalog.push((search_id, name));
+                engine.insert(search_id, &name);
+                catalog.push((search_id, name));
 
-    //             search_id += 1;
-    //         }
-    //         if i > 2 {
-    //             continue;
-    //         }
-    //     }
-    // }
+                search_id += 1;
+            }
+            if i > 2 {
+                continue;
+            }
+        }
+    }
 
     loop {
         print!("Текст для поиска: ");
